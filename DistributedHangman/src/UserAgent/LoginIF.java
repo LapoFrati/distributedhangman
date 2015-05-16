f7beb26eb68866327cfb6d7abdb94271d6417395
LapoFrati <lapo.frati.git@gmail.com>
2015-05-16 23:52:27 +0100

    Renamed the interfaces adding IF to better identify them

--- DistributedHangman/src/UserAgent/LoginIF.java
+++ DistributedHangman/src/UserAgent/LoginIF.java
@@ -5,9 +5,9 @@ import java.rmi.RemoteException;
 import java.rmi.server.ServerNotActiveException;
 import java.security.PublicKey;
 
-public interface Login extends Remote {
+public interface LoginIF extends Remote {
 	public boolean isRegisteredUser(String userName) throws RemoteException;
-	public boolean logIn(String userName, byte[] publicKeyEncryptedPassword) throws RemoteException, ServerNotActiveException;
+	public boolean logIn(String userName, byte[] publicKeyEncryptedPassword, Object callback) throws RemoteException, ServerNotActiveException;
 	public boolean registerNewUser(String userName, byte[] publicKeyEncryptedPassword) throws RemoteException, ServerNotActiveException;
 	public boolean logOut(String userName) throws RemoteException, ServerNotActiveException;
 	public PublicKey getPublicKey() throws RemoteException;
