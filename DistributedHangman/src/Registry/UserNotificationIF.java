9d518ef601c47578a8f7b7e6f10b9b198dad2ede
LapoFrati <lapo.frati.git@gmail.com>
2015-05-21 14:38:35 +0200

    Change all packages' names to lowercase

--- DistributedHangman/src/Registry/UserNotificationIF.java
+++
@@ -1,8 +0,0 @@
-package Registry;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface UserNotificationIF extends Remote{
-	public void notifyUser(String msg) throws RemoteException;
-}
