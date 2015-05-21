23f6b902faa13656549c62ff427673442f801618
LapoFrati <lapo.frati.git@gmail.com>
2015-05-21 21:44:08 +0200

    Add json fields

--- DistributedHangman/src/messages/JSONCodes.java
+++ DistributedHangman/src/messages/JSONCodes.java
@@ -5,6 +5,13 @@ public class JSONCodes {
 								master 				= "MASTER",
 								guesser				= "GUESSER",
 								roomName			= "ROOMNAME",
-								numberOfGuessers	= "NUMBER_OF_GUESSERS";
+								numberOfGuessers	= "NUMBER_OF_GUESSERS",
+								serverIP			= "SERVER_IP",
+								registryName		= "REGISTRY_NAME",
+								registryPort		= "REGISTRY_PORT",
+								serverPort			= "SERVER_PORT",
+								maxNumberOfGames	= "MAX_NUM_GAMES",
+								baseMulticastAddr	= "BASE_MULT_ADDR",
+								maxMulticastAddr	= "MAX_MULT_ADDR";
 	
 }
