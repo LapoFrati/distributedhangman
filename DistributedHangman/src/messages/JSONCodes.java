e3dcb56cd45a4bc9b42fb0e629f92ae1cd6cf94b
LapoFrati <lapo.frati.git@gmail.com>
2015-05-21 14:37:47 +0200

    Add class for the JSONFields and TCP/UDP messages to keep them organized

---
+++ DistributedHangman/src/messages/JSONCodes.java
@@ -0,0 +1,10 @@
+package messages;
+
+public class JSONCodes {
+	public static final String 	role 				= "ROLE",
+								master 				= "MASTER",
+								guesser				= "GUESSER",
+								roomName			= "ROOMNAME",
+								numberOfGuessers	= "NUMBER_OF_GUESSERS";
+	
+}
---
+++ DistributedHangman/src/messages/TCPmsg.java
@@ -0,0 +1,7 @@
+package messages;
+
+public class TCPmsg {
+	public static final String 	ack 				= "ACK",
+								connectionClosed 	= "SOCKET_CLOSED";
+								
+}
