9ee472b224444e97fe9820de9fbc08c776aa9a93
LapoFrati <lapo.frati.git@gmail.com>
2015-05-27 09:55:59 +0200

    Move notifyall functionality inside the lock

--- DistributedHangman/src/registry/WaitingRoom.java
+++ DistributedHangman/src/registry/WaitingRoom.java
@@ -1,61 +1,41 @@
 package registry;
 
-import java.util.Vector;
-
 public class WaitingRoom {
-	private String roomName, password, multicastAddress;
+	private String roomName;
 	private int currentGuessers, requiredGuessers;
-	private Vector<String> guessers;
 	private Object 	numberOfGuessersLock	= new Object();
 	private WaitingRoomLock roomWaitLock;
 	
-	public WaitingRoom(String roomName, int requiredPlayers, String password, String multicastAddress, WaitingRoomLock roomWait){
+	public WaitingRoom(String roomName, int requiredPlayers, WaitingRoomLock roomWait){
 		this.roomName = roomName;
 		this.requiredGuessers = requiredPlayers;
 		this.currentGuessers = 0;
-		this.password = password;
-		this.multicastAddress = multicastAddress;
 		this.roomWaitLock = roomWait;
-		guessers = new Vector<String>();
+	}
+	
+	public void setPassword(String password){
+		roomWaitLock.setPassword(password);
+	}
+	
+	public void setMulticast(String multicast){
+		roomWaitLock.setMulticast(multicast);
 	}
 	
 	public String getRoomInfo(){
 		return roomName+": "+currentGuessers+"/"+requiredGuessers;
 	}
 	
-	public String getPassword(){
-		return this.password;
-	}
-	
-	public String getMulticastAddress(){
-		return this.multicastAddress;
-	}
-	
 	public WaitingRoomLock getWaitLock(){
 		return roomWaitLock;
 	}
 	
-	public boolean removeGuesser(String exGuesser){
-		boolean result;
+	public void removeGuesser(){
 		synchronized (numberOfGuessersLock) {
-			if(guessers.remove(exGuesser)){
 				currentGuessers--;
-				result = true;
-			} else {
-				result = false;
-			}
-		}
-		return result;
-	}
-	
-	public void removeMaster(){
-		synchronized (roomWaitLock) {
-			// wake up all waiting threads
-			roomWaitLock.notifyAll();
 		}
 	}
 	
-	public boolean addGuesser(String userName){
+	public boolean addGuesser(){
 		boolean result;
 		
 		synchronized (numberOfGuessersLock) {
@@ -63,19 +43,12 @@ public class WaitingRoom {
 			if(currentGuessers >= requiredGuessers)
 				result = false;
 			else{
-				guessers.addElement(userName);
 				currentGuessers++;
 				result = true;
 			}
-			
-			if(result && currentGuessers == requiredGuessers){
-				synchronized (roomWaitLock) {
-					roomWaitLock.setGameStarting();
-					// game is ready to start, wake up all waiting threads
-					roomWaitLock.notifyAll();
-				}
-			}
+			if(result && currentGuessers == requiredGuessers)
+				roomWaitLock.setGameStarting();
 		}
-		return result;
+	return result;
 	}
 }
