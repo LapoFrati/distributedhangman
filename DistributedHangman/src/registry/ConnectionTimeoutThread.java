a5fdb41c520b83b3ab341f697b457289669c6d4a
LapoFrati <lapo.frati.git@gmail.com>
2015-05-27 13:43:50 +0200

    Simplify connectionTimeout

--- DistributedHangman/src/registry/ConnectionTimeoutThread.java
+++ DistributedHangman/src/registry/ConnectionTimeoutThread.java
@@ -3,34 +3,36 @@ package registry;
 import java.io.IOException;
 import java.net.Socket;
 
+import org.json.simple.JSONObject;
+
 import messages.JSONCodes;
 
 public class ConnectionTimeoutThread extends Thread {
 	Socket socketToBeClosed;
-	long currentTime, alarmTime;
-	public volatile boolean alarmActivated = true;
+	long currentTime, milliSecondsToSleep;
+	Thread threadToWakeup;
+	public volatile boolean disableInterrupt = false;
 	
-	public ConnectionTimeoutThread(Socket socket, long secondsToSleep) {
+	public ConnectionTimeoutThread(Socket socket, long secondsToSleep, Thread thread) {
 		this.socketToBeClosed = socket;
 		this.currentTime = System.currentTimeMillis(); 
-		this.alarmTime = currentTime + secondsToSleep*1000;
+		this.milliSecondsToSleep = secondsToSleep*1000;
+		this.threadToWakeup = thread;
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void run() {
-		byte[] closingMessage = JSONCodes.connectionClosed.getBytes();
-		while(alarmActivated){
+		JSONObject closingJSON = new JSONObject();
+		closingJSON.put(JSONCodes.message, JSONCodes.connectionClosed);
+		byte[] closingMessage = closingJSON.toJSONString().getBytes();
+		
 			try {
-				Thread.sleep(alarmTime - currentTime);
-				alarmActivated = false;
-			} catch (InterruptedException e) {
-				if(alarmActivated){
-					currentTime = System.currentTimeMillis();
-					if(currentTime > alarmTime)
-						alarmActivated = false;
-				}
-			}
-		}
+				Thread.sleep(milliSecondsToSleep);
+			} catch (InterruptedException e){}
+		
+		if(!disableInterrupt)
+			threadToWakeup.interrupt();
 		
 		try {
 			socketToBeClosed.getOutputStream().write(closingMessage);
