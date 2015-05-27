53c008a85ac881e7efabb43df40dd88ae19912ff
LapoFrati <lapo.frati.git@gmail.com>
2015-05-27 09:56:50 +0200

    Add notifyAll to checkIfGameStarting. Use boolean value to notify only once

--- DistributedHangman/src/registry/WaitingRoomLock.java
+++ DistributedHangman/src/registry/WaitingRoomLock.java
@@ -1,15 +1,34 @@
 package registry;
 
 public class WaitingRoomLock {
-	private boolean gameStarting;
+	private boolean gameStarting, usersNotified;
+	private String roomPassword;
+	private String roomMulticast;
 	
 	public WaitingRoomLock(){
 		gameStarting = false;
+		usersNotified = false;
 	}
 	public void setGameStarting(){
 		gameStarting = true;
 	}
 	public boolean checkIfGameStarting(){
+		if(gameStarting && !usersNotified){
+			usersNotified = true;
+			this.notifyAll();
+		}
 		return gameStarting;
+	}
+	public void setPassword(String pass){
+		this.roomPassword = pass;
+	}
+	public String getPassword(){
+		return this.roomPassword;
+	}
+	public void setMulticast(String multicast){
+		this.roomMulticast = multicast;
+	}
+	public String getMulticast(){
+		return this.roomMulticast;
 	}
 }
