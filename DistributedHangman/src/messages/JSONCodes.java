87756aa1d3f7870475443d1939c0b05c5cd74077
LapoFrati <lapo.frati.git@gmail.com>
2015-06-25 19:47:42 +0200

    Organize codes. Sort them by usage area.

--- DistributedHangman/src/messages/JSONCodes.java
+++ DistributedHangman/src/messages/JSONCodes.java
@@ -2,10 +2,13 @@ package messages;
 
 public class JSONCodes {
 	public static final String 	role 				= "ROLE",
-								master 				= "MASTER",
-								guesser				= "GUESSER",
+									master 				= "MASTER",
+									guesser				= "GUESSER",
+								
 								roomName			= "ROOMNAME",
 								numberOfGuessers	= "NUMBER_OF_GUESSERS",
+								
+								// CONFIGURATION INFO
 								serverIP			= "SERVER_IP",
 								registryName		= "REGISTRY_NAME",
 								registryPort		= "REGISTRY_PORT",
@@ -13,6 +16,9 @@ public class JSONCodes {
 								maxNumberOfGames	= "MAX_NUM_GAMES",
 								baseMulticastAddr	= "BASE_MULT_ADDR",
 								maxMulticastAddr	= "MAX_MULT_ADDR",
+								
+								// GAME CREATION
+								// TODO CHECK CODES
 								connectionClosed 	= "SOCKET_CLOSED",
 								waitingRoomsFull	= "WAITINGROOMS_FULL",
 								newRoomCreated		= "NEW_ROOM_CREATED",
@@ -21,6 +27,26 @@ public class JSONCodes {
 								roomClosed			= "ROOM_CLOSED",
 								message				= "MSG",
 								userName			= "USERNAME",
-								gameStarting		= "GAME_STARTING";
+								gameStarting		= "GAME_STARTING",
+								roomMulticast		= "ROOM_MULTICAST_ADDR",
+								roomPassword		= "ROOM_PASSWORD",
+								cleanupCompleted	= "CLEANUP_COMPLETED",
+								timeoutLength		= "TIMEOUT_LEN",
+								
+								// GAME MANAGEMENT
+								guesserTimeout		= "GUESSER_TIMEOUT",
+
+								ack					= "ACK",
+								word				= "WORD",
+								guess				= "GUESS",
+								
+								gameStatus			= "GAME_STATUS",
+									correctGuess		= "CORRECT_GUESS",
+									wrongGuess			= "WRONG_GUESS",
+									repeatedGuess		= "REPEATED_GUESS",
+									masterLost			= "MASTER_LOST",
+									masterWon			= "MASTER_WON",
+									masterLeft			= "MASTER_LEFT";
+								
 	
 }
