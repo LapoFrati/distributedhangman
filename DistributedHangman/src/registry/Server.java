d49d85b05a971c054b262172b8bd225d1025ba7d
LapoFrati <lapo.frati.git@gmail.com>
2015-07-08 14:33:27 +0200

    Rename new Server class

--- DistributedHangman/src/registry/Server.java
+++ DistributedHangman/src/registry/Server.java
@@ -1,14 +1,9 @@
 package registry;
 
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-
-import messages.ConfigurationException;
 import messages.JSONCodes;
 import messages.ReadConfigurationFile;
 
@@ -17,36 +12,31 @@ public class Server {
 	
 	public static void main(String[] args) throws IOException, NoSuchAlgorithmException, ClassNotFoundException {
 		config = new ReadConfigurationFile("serverConfig.json");
-    	String 	serverIP 				= config.getJsonField(JSONCodes.serverIP),
+    	
+		String 	serverIP 				= config.getJsonField(JSONCodes.serverIP),
 				registryName 			= config.getJsonField(JSONCodes.registryName),
 				registryPort 			= config.getJsonField(JSONCodes.registryPort),
+				masterServerPort   		= config.getJsonField(JSONCodes.masterServerPort),
+				guesserServerPort 		= config.getJsonField(JSONCodes.guesserServerPort),
 				baseMulticastAddr		= config.getJsonField(JSONCodes.baseMulticastAddr),
 				maxMulticastAddr		= config.getJsonField(JSONCodes.maxMulticastAddr);
-			int	maxNumberOfWaitingRooms = Integer.valueOf(config.getJsonField(JSONCodes.maxNumberOfGames)),
-				timeoutLength			= Integer.valueOf(config.getJsonField(JSONCodes.timeoutLength));
+		
+    	int	maxNumberOfWaitingRooms = Integer.valueOf(config.getJsonField(JSONCodes.maxNumberOfGames)),
+			timeoutLength			= Integer.valueOf(config.getJsonField(JSONCodes.timeoutLength));
+			
+		MasterConnectionListener masterConnectionListener 		= new MasterConnectionListener(masterServerPort, timeoutLength);
+	    GuesserConnectionListener guesserConnectcionListener 	= new GuesserConnectionListener(guesserServerPort, timeoutLength);
+
     	
     	// Start registry for RMI
     	MyRegistry myLogin = new MyRegistry(maxNumberOfWaitingRooms,baseMulticastAddr,maxMulticastAddr);
-        ServerSocket serverSocket = null;
         System.setProperty("java.rmi.server.hostname", serverIP);
         Registry reg = LocateRegistry.createRegistry(Integer.parseInt(registryPort));
         reg.rebind(registryName, myLogin);
         
-        try {
-            serverSocket = new ServerSocket(4444);
-        } catch (IOException e) {
-            throw new ConfigurationException("Could not listen on port: 4444.");
-        }
+        masterConnectionListener.start();
+        guesserConnectcionListener.start();
         
         System.out.println("Server Listening");
-        
-        Executor myPool = Executors.newCachedThreadPool();
-        try{
-        	while(true){
-        		myPool.execute(new ServerThread(serverSocket.accept(), timeoutLength));
-        		}
-        }finally{
-        	serverSocket.close();
-        }
     }
 }
