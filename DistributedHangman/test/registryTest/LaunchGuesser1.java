17a20c3481b48a084a915be0266ada079ae3a023
LapoFrati <lapo.frati.git@gmail.com>
2015-07-08 14:31:18 +0200

    Update test classes

--- DistributedHangman/test/registryTest/LaunchGuesser1.java
+++ DistributedHangman/test/registryTest/LaunchGuesser1.java
@@ -4,14 +4,15 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.rmi.NotBoundException;
+import java.rmi.server.ServerNotActiveException;
 
-import userAgent.User;
+import userAgent.UserLogin;
 
 public class LaunchGuesser1 {
-	public static void main(String[] args) throws ClassNotFoundException, IOException, NotBoundException {
+	public static void main(String[] args) throws ClassNotFoundException, IOException, NotBoundException, ServerNotActiveException {
 		String inputString = "Test\nqwerty\ng\nLapo\n";
 		InputStream inStream = new ByteArrayInputStream(inputString.getBytes());
 		System.setIn(inStream);
-		User.main(null);
+		UserLogin.main(null);
 	}
 }
--- DistributedHangman/test/registryTest/LaunchMaster.java
+++ DistributedHangman/test/registryTest/LaunchMaster.java
@@ -4,14 +4,15 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.rmi.NotBoundException;
+import java.rmi.server.ServerNotActiveException;
 
-import userAgent.User;
+import userAgent.UserLogin;
 
 public class LaunchMaster {
-	public static void main(String[] args) throws ClassNotFoundException, IOException, NotBoundException {
+	public static void main(String[] args) throws ClassNotFoundException, IOException, NotBoundException, ServerNotActiveException {
 		String inputString = "Lapo\nFrati\nm\n2\n";
 		InputStream inStream = new ByteArrayInputStream(inputString.getBytes());
 		System.setIn(inStream);
-		User.main(null);
+		UserLogin.main(null);
 	}
 }
