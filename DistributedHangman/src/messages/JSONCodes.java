e52c494b1be6a74db27ef4a0ee4a4f5c02206f02
LapoFrati <lapo.frati.git@gmail.com>
2015-07-14 20:01:21 +0200

    Update codes

--- DistributedHangman/src/messages/JSONCodes.java
+++ DistributedHangman/src/messages/JSONCodes.java
@@ -12,7 +12,6 @@ public class JSONCodes {
 								serverIP			= "SERVER_IP",
 								registryName		= "REGISTRY_NAME",
 								registryPort		= "REGISTRY_PORT",
-								serverPort			= "SERVER_PORT", // TODO remove
 								masterServerPort	= "MASTER_SERVER_PORT",
 								guesserServerPort	= "GUESSER_SERVER_PORT",
 								maxNumberOfGames	= "MAX_NUM_GAMES",
