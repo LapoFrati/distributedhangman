712f0f6ead91ce5219a50004f75d2fbb8a5ef945
LapoFrati <lapo.frati.git@gmail.com>
2015-07-18 13:09:16 +0200

    Add abstract class for guesser/master launcher. Mirrors the structure in the server side.

---
+++ DistributedHangman/src/userAgent/AbstractUserLauncher.java
@@ -0,0 +1,53 @@
+package userAgent;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public abstract class AbstractUserLauncher {
+	protected Socket serverSocket;
+	protected PrintWriter out;
+    protected BufferedReader in;
+    protected ExitListenerThread exitListener;
+    private boolean listenerStopped;
+    
+	public AbstractUserLauncher(BufferedReader stdIn, String serverIP, String masterServerPort){
+		try {
+			serverSocket = new Socket(InetAddress.getByName(serverIP), Integer.parseInt(masterServerPort));
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			out = new PrintWriter(serverSocket.getOutputStream(), true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			in = new BufferedReader( new InputStreamReader(serverSocket.getInputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		exitListener = new ExitListenerThread(out, in, stdIn, this);
+		listenerStopped = true;
+	}
+	
+	protected void stopListener(){
+		if(listenerStopped == false){
+			listenerStopped = true;
+			exitListener.interrupt();
+		}
+	}
+	
+	protected void startListener(){
+		if(listenerStopped == true){
+			listenerStopped = false;
+			exitListener.start();
+		}	}
+	
+	public abstract void notifyExit();
+	public abstract void checkState();
+}
