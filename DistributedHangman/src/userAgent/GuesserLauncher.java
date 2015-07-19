3cceb516dca93ee26593e4c3617e5a9f6c1cfa31
LapoFrati <lapo.frati.git@gmail.com>
2015-07-19 18:53:20 +0200

    Fix master/guesser launcher, taking advantage of the abstract class.

--- DistributedHangman/src/userAgent/GuesserLauncher.java
+++ DistributedHangman/src/userAgent/GuesserLauncher.java
@@ -2,77 +2,116 @@ package userAgent;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 import messages.JSONCodes;
-
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class GuesserLauncher {
-	@SuppressWarnings("unchecked")
-	public void startGuesser(BufferedReader stdIn, String userName, String serverIP, String guesserServerPort) throws NumberFormatException, UnknownHostException, IOException{
+public class GuesserLauncher extends AbstractUserLauncher{
+	BufferedReader stdIn;
+	String userName;
+	boolean exitReceived;
+	
+	public GuesserLauncher(BufferedReader stdIn, String userName,String serverIP, String masterServerPort) {
+		super(stdIn, serverIP, masterServerPort);
+		this.stdIn = stdIn;
+		this.userName = userName;
+		exitReceived = false;
+	}
+
+	public void startGuesser() throws NumberFormatException, UnknownHostException, IOException{
 		String 	password 	= "", 
-    			multicast 	= "";
-		Boolean keepReading = true;
-		JSONObject messageToServer, messageFromServer = null;
-		messageToServer = new JSONObject();
-		messageToServer.put(JSONCodes.role, JSONCodes.guesser);
-		messageToServer.put(JSONCodes.userName, userName);
+    			multicast 	= "",
+    			roomToJoin	= "", 
+    			rawMessage	= "";
+		Boolean gameStarting = false;
+		JSONObject messageFromServer = null;
 		
-		try (	// Create a socket to connect to the server using the info in the configuration file
-                Socket serverSocket = new Socket(InetAddress.getByName(serverIP), Integer.parseInt(guesserServerPort));
-                
-        		// TCP socket provides bidirectional I/O. Attach in/out to the respective streams.
-        		PrintWriter out =
-                    new PrintWriter(serverSocket.getOutputStream(), true);
-                BufferedReader in =
-                    new BufferedReader(
-                        new InputStreamReader(serverSocket.getInputStream()));
-            ){
-			out.println(messageToServer);
+		try{
+
+			out.println(userName); // send userName to start receiving WaitingRooms' updates
 			
 			System.out.println("Choose room to join: ");
-			messageToServer.put(JSONCodes.roomName, stdIn.readLine());
-			out.println(messageToServer);
-			while(keepReading){
-				try {
-					messageFromServer = (JSONObject) new JSONParser().parse(in.readLine());
-					
-				} catch (ParseException e) {
-					System.out.println("Message:" + messageFromServer);
-					e.printStackTrace();
+			roomToJoin = stdIn.readLine();
+			out.println(roomToJoin);
+			
+			initializationLoop:
+			while(true){
+				try{
+				rawMessage = in.readLine();
+				//System.out.println(rawMessage);
+				} catch (SocketException e){
+					// notifyExit closes in to stop the readline
 				}
-				switch((String) messageFromServer.get(JSONCodes.message)){
+				checkState();
 				
-					case JSONCodes.roomJoined: 			System.out.println("Joined selected room, please wait for game to start");
-														break;
-					case JSONCodes.connectionClosed:	System.out.println("Game is starting");
-														keepReading = false;
-														break;
+				if(rawMessage != null){
+					try {
+						messageFromServer = (JSONObject) new JSONParser().parse(rawMessage);
+					} catch (ParseException e) {
+						System.out.println("Message:" + messageFromServer);
+						e.printStackTrace();
+					}
+				} else {
+					System.out.println("null read");
+					break;
+				}
+				
+				checkState();
+				
+				switch((String) messageFromServer.get(JSONCodes.message)){
+					case JSONCodes.roomJoined:			System.out.println("Room joined. Waiting for the game to start. Type \"exit\" to quit.");
+														exitListener.start(); 
+														continue initializationLoop;
+					case JSONCodes.connectionClosed:	System.out.println("Timeout expired. Game aborted.");
+														break initializationLoop;
 					case JSONCodes.gameStarting: 		password  = (String) messageFromServer.get(JSONCodes.roomPassword);
 		    											multicast = (String) messageFromServer.get(JSONCodes.roomMulticast);
-		    											break;
-					case JSONCodes.roomClosed:			System.out.println("Room closed.");
-					case JSONCodes.guesserJoinError:	System.out.println("Choose room to join: ");
-														messageToServer.put(JSONCodes.roomName, stdIn.readLine());
-														out.println(messageToServer);
-		    											break;
+		    											gameStarting = true;
+		    											break initializationLoop;
+					case JSONCodes.roomClosed:			System.out.println("Room closed. Game aborted.");
+														break initializationLoop;
+					case JSONCodes.guesserJoinError:	System.out.println("Couldn't join the room.\nPlease choose another one: ");
+														roomToJoin = stdIn.readLine();
+														out.println(roomToJoin);
+		    											continue initializationLoop;
 				}		
 			} 
-		} catch (UnknownHostException e) {
-            System.out.println("Don't know about host " + InetAddress.getByName(serverIP));
-        } catch (IOException e) {
-            System.out.println("Couldn't get I/O for the connection to " +
-            		InetAddress.getByName(serverIP));
-        }
-		System.out.println(password + "-" + multicast);
-		GuesserReceiver guesser = new GuesserReceiver(userName, password,multicast);
-		guesser.startGame();
+		} catch ( TerminationException e ) {
+			
+		} finally {
+			stopListener();
+    		if(!serverSocket.isClosed())
+    			serverSocket.close();
+		}
+		
+		if(gameStarting && !exitReceived){
+			System.out.println(password + "-" + multicast);
+			GuesserReceiver guesser = new GuesserReceiver(userName, password, multicast);
+			guesser.startGame();
+		}
+	}
+
+	@Override
+	public void notifyExit() {
+		exitReceived = true;
+		out.println("exit"); // send closing message to the server
+		stopListener(); // prevent further notifications
+		try {
+			serverSocket.close(); // stops the possible wait on in.readLine
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+
+	@Override
+	public void checkState() {
+		if(exitReceived == true){
+			System.out.println("Game Terminated.");
+			throw new TerminationException("GuesserLauncher received exit request.");
+		}
 	}
 }
--- DistributedHangman/src/userAgent/MasterLauncher.java
+++ DistributedHangman/src/userAgent/MasterLauncher.java
@@ -2,136 +2,202 @@ package userAgent;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
 import java.net.UnknownHostException;
 
 import messages.JSONCodes;
-
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class MasterLauncher {
+public class MasterLauncher extends AbstractUserLauncher{
+	BufferedReader stdIn;
+	String userName;
+	boolean exitReceived;
+	
+	public MasterLauncher(BufferedReader stdIn, String userName, String serverIP, String masterServerPort) {
+		super(stdIn, serverIP, masterServerPort);
+		this.stdIn = stdIn;
+		this.userName = userName;
+		exitReceived = false;
+	}
+
 	@SuppressWarnings("unchecked")
-	public void startMaster(BufferedReader stdIn, String userName, String serverIP, String masterServerPort) throws NumberFormatException, UnknownHostException, IOException{
+	public void startMaster() throws NumberFormatException, UnknownHostException, IOException{
 		JSONObject 	messageToServer, 
 					messageFromServer = null;
 		
     	String 	password 	= "", 
     			multicast 	= "",
     			targetword 	= "",
-				reply 		= "";
+				message		= "";
     	
     	int 	numberOfGuessers = -1,
     			numberOfAttempts = -1;
 		
     	boolean readNumber 	= true,
-				keepReading = true,
-				attemptsOk 	= false,
-				proceed 	= false;
+				gameStarting = true;
     	
-    	Thread terminationThread;
+    	try {
+				messageToServer = new JSONObject();
+				
+				// get number of guessers
+        		System.out.println("Required number of guessers: ");
+        		while(readNumber){
+        			try{
+        			numberOfGuessers = Integer.valueOf(stdIn.readLine());
+        			readNumber = false;
+        			}catch (NumberFormatException e) {
+						System.out.println("Wrong number format.");
+					}
+        		}
+        		
+        		// send necessary info to create a waitingRoom
+        		messageToServer.put(JSONCodes.roomName, userName);
+        		messageToServer.put(JSONCodes.numberOfGuessers, numberOfGuessers);
+        		out.println(messageToServer);
+				
+        		System.out.println("Request sent");
+        		
+        		// start the exitListener thread
+        		startListener();
+        		
+    			try{
+    				message = in.readLine();
+    			} catch (IOException e){
+    				// notifyExit closes in to stop the readline
+    			}
+    			
+    			checkState(); // Throws TerminationException
+    			
+        		try {
+					messageFromServer = (JSONObject) new JSONParser().parse(message);	
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+        		
+    			switch((String) messageFromServer.get(JSONCodes.message)){
+    				case JSONCodes.gameStarting:		password  = (String) messageFromServer.get(JSONCodes.roomPassword);
+	        											multicast = (String) messageFromServer.get(JSONCodes.roomMulticast);
+	        											gameStarting = true;
+	        											System.out.println("Game is starting");
+	        											break;	
+    				case JSONCodes.connectionClosed: 	System.out.println("Connection closed. Game aborted"); // server side timeout expired
+    													gameStarting = false;
+    													break;
+    			}
+        		
+        		
+    	} catch (TerminationException e){
+    		//System.out.println(e.getMessage());
+    		/* CheckStates has forced the program to exit the loop throwing this exception */
+    	} finally {
+    		stopListener(); // close the listener if no exit request has been received
+    		if(!serverSocket.isClosed())
+    			serverSocket.close();
+    	}
     	
-    	try (	// Create a socket to connect to the server using the info in the configuration file
-                Socket serverSocket = new Socket(InetAddress.getByName(serverIP), Integer.parseInt(masterServerPort));
-                
-        		// TCP socket provides bidirectional I/O. Attach in/out to the respective streams.
-        		PrintWriter out =
-                    new PrintWriter(serverSocket.getOutputStream(), true);
-                BufferedReader in =
-                    new BufferedReader(
-                        new InputStreamReader(serverSocket.getInputStream()));
-            ){
-    				messageToServer = new JSONObject();
-    				terminationThread = new MasterLauncherTerminationThread(out, stdIn, in);
-		        	
-	        		System.out.println("Required number of guesser: ");
-	        		while(readNumber){
-	        			try{
-	        			numberOfGuessers = Integer.valueOf(stdIn.readLine());
-	        			readNumber = false;
-	        			}catch (NumberFormatException e) {
-							System.out.println("Wrong number format.");
-						}
-	        		}
-    				messageToServer.put(JSONCodes.role, JSONCodes.master);
-	        		messageToServer.put(JSONCodes.roomName, userName);
-	        		messageToServer.put(JSONCodes.numberOfGuessers, numberOfGuessers);
-	        		
-	        		System.out.println("Sending room info");
-	        		
-	        		out.println(messageToServer);
-					
-	        		System.out.println("Waiting for reply");
-	        		
-	        		// start the termination thread
-	        		terminationThread.run();
-	        		
-	        		while(keepReading){
-		        		try {
-							messageFromServer = (JSONObject) new JSONParser().parse(in.readLine());	
-						} catch (ParseException e) {
-							e.printStackTrace();
-						}
-	        			switch((String) messageFromServer.get(JSONCodes.message)){
-	        				case JSONCodes.waitingRoomsFull: 	System.out.println("Waiting to create a new room");
-	        													break;
-	        				case JSONCodes.newRoomCreated:		System.out.println("New room created. Waiting for guessers.");
-	        													break;
-	        				case JSONCodes.gameStarting:		password  = (String) messageFromServer.get(JSONCodes.roomPassword);
-			        											multicast = (String) messageFromServer.get(JSONCodes.roomMulticast);
-			        											break;	
-	        				case JSONCodes.connectionClosed: 	keepReading = false;
-	        													System.out.println("Connection closed.");
-	        													break;
-	        			}
-	        		}
-	        		
-	        		System.out.println(password + "-" + multicast);
-	        		
-        			System.out.println("Starting Master");
-        			
-        			System.out.println("Choose the target word");
-        			
-        			do{
-        				targetword = stdIn.readLine();
-        				System.out.println("The chosen word is: " + targetword+".");
-        				do{
-        					System.out.println("Proceed? [Y/N]");
-        					reply = stdIn.readLine(); 
-        				}while(reply.matches("y|Y|n|N"));
-        				
-        				if(reply.matches("n|N")){
-        					proceed = false;
-        				} else {
-        					proceed = true;
-        				}
-        			}
-        			while(!proceed);
-        			
-        			System.out.println("Choose number of allowed attempts.");
-        			while(!attemptsOk){
-        				try {
-        					numberOfAttempts = Integer.valueOf(stdIn.readLine());
-						} catch (Exception e) {}
-        				if(numberOfAttempts >0 ){
-        					attemptsOk = true;
-        				} else {
-        					attemptsOk = false;
-        				}
-        			}
-    	} catch (UnknownHostException e) {
-            System.out.println("Don't know about host " + InetAddress.getByName(serverIP));
-        } catch (IOException e) {
-            System.out.println("Couldn't get I/O for the connection to " +
-            		InetAddress.getByName(serverIP));
-        }
-        			MasterWorker master = new MasterWorker(password, multicast, targetword, numberOfAttempts, numberOfGuessers);
-        			master.startGame();
-    			  
+    	if(gameStarting && !exitReceived){
+    		System.out.println(password + "-" + multicast);
+    		targetword = getWord();
+			numberOfAttempts = getAttempts();
+			MasterWorker master = new MasterWorker(password, multicast, targetword, numberOfAttempts, numberOfGuessers);
+	    	master.startGame();
+		}  
 	}
-	
+    
+
+	public String getWord(){
+    	String targetWord = "", reply = "";
+    	boolean proceed = false;
+    	
+    	System.out.println("Choose the target word");
+		do{
+			try {
+				targetWord = stdIn.readLine().toLowerCase();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if(targetWord.length() == 0){
+				System.out.println("Choose the target word");
+				continue;
+			}
+			System.out.println("The chosen word is: " + targetWord+".");
+			do{
+				System.out.println("Proceed? [Y/N]");
+				try {
+					reply = stdIn.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} 
+			}while(!reply.matches("y|Y|n|N"));
+			
+			if(reply.matches("n|N")){
+				proceed = false;
+				System.out.println("Choose the target word");
+			} else {
+				proceed = true;
+			}
+		} while(!proceed);
+		
+		return targetWord;
+    }
+        
+    public int getAttempts(){
+    	Boolean attemptsOk = false;
+    	int numberOfAttempts = 0;
+    	String reply = ""; 
+    	
+    	System.out.println("Choose number of allowed attempts:");
+		while(!attemptsOk){
+			try {
+				numberOfAttempts = Integer.valueOf(stdIn.readLine());
+			} catch (Exception e) {
+				System.out.println("Enter only numbers, please.");
+				continue;
+			}
+			if(numberOfAttempts > 0 ){
+				System.out.println("The chosen number of attempts is: " + numberOfAttempts+".");
+				do{
+					System.out.println("Proceed? [Y/N]");
+					try {
+						reply = stdIn.readLine();
+					} catch (IOException e) {
+						e.printStackTrace();
+					} 
+				}while(!reply.matches("y|Y|n|N"));
+				
+				if(reply.matches("n|N")){
+					attemptsOk = false;
+					System.out.println("Choose number of allowed attempts:");
+				} else {
+					attemptsOk = true;
+				}
+			} else {
+				System.out.println("Enter a number greater than 0.");
+				attemptsOk = false;
+			}
+		}
+		
+		return numberOfAttempts;
+    }    
+
+	@Override
+	public void notifyExit() {
+		exitReceived = true;
+		out.println("exit"); // send closing message to the server
+		stopListener(); // prevent further notifications
+		try {
+			serverSocket.close(); // stops the possible wait on in.readLine
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+
+	@Override
+	public void checkState() {
+		if(exitReceived == true){
+			System.out.println("Game Terminated.");
+			throw new TerminationException("MasterLauncher received exit request.");
+		}
+	}
 }
